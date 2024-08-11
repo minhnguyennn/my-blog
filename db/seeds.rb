70c9f5de70da9c223e2983d2cdbffcf344edd6b1
minhnguyennn <minh842657913@gmail.com>
2024-08-11 13:59:12 +0700

    Add devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+User.create(email: 'minh@gmail.com', password: '123456', password_confirmation: '123456', name: "Nguyen Van Minh")
+
 10.times do |x|
-  Post.create(title: "Title #{x}", body: "Body #{x} word go here")
+  Post.create(title: "Title #{x}", body: "Body #{x} word go here", user_id: User.first.id)
 end
